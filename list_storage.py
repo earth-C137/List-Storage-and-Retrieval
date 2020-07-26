56112bd936e53669aa8b34a2afc2e9612ab1dca0
earth-C137 <42741144+earth-C137@users.noreply.github.com>
2020-07-26 11:53:10 -0500

    Update list_storage.py
    Made some minor changes. Mostly formatting, and I removed the built in file extension.  I didn't like that feature.

--- list_storage.py
+++ list_storage.py
@@ -3,93 +3,100 @@ A small module I made to save lists to files for future use.
 There are two classes and multiple methods.
 """
 
-#The pickle module can only be used in python.
+# The pickle module can only be used in python.
 import pickle
-#So you can use the more universal json format.
+# So you can use the more universal json format.
 import json
 
 
 class StoreList():
-#This class requires one argument, a list, on creation.
+# This class requires one argument, a list, on creation.
     def __init__(self, list):
-
         self.list = list
 
-#Now you can send it to any file in any method as many times as you want.
-#Just use a file path as file name.  The extension is already added.
-#The file_name must be entered as a string.
-#file_name example: '/home/user/Desktop/example_list'
+# Now you can send it to any file in any method as many times as you want.
+# Just use a file path as file name.
+# The file_name must be entered as a string.
+# file_name example: '/home/user/Desktop/example_list.txt'
 
-    #***single line
+#***Single line, aka iterator loop.
     def simplesend(self, file_name):
-        with open('{}.txt'.format(file_name), 'w') as filehandle:
+        with open('{}'.format(file_name), 'a+') as filehandle:
             for listitem in self.list:
                 filehandle.write('%s\n' % listitem)
 
-    #***multi line
+#***Multi line
     def multisend(self, file_name):
-        with open('{}.txt'.format(file_name), 'w') as filehandle:
+        with open('{}'.format(file_name), 'a+') as filehandle:
             filehandle.writelines("%s\n" % place for place in self.list)
 
-    #***python only pickle module
+#***Python only pickle module.
+# Typically use a .pickle extension.
     def pklsend(self, file_name):
-        with open('{}.data'.format(file_name), 'wb') as filehandle:
-            # store the data as binary data stream
+        with open('{}'.format(file_name), 'wb') as filehandle:
+            # Store the data as binary data stream.
             pickle.dump(self.list, filehandle)
 
-    #***json format, very universal
+#***JSON format, very universal.
     def jsend(self, file_name):
-        # open output file for writing
-        with open('{}.txt'.format(file_name), 'w') as filehandle:
+        with open('{}'.format(file_name), 'w') as filehandle:
             json.dump(self.list, filehandle)
 
-            
-#Store and retrieve are seperate because it would use the same list and cause problems.
-#Now you can retrieve anyway as a one liner right to a list or print.
+# Seperated store and retrieve
+# because it would use the same list and cause problems.
+# Now you can retrieve anyway as a one liner right to a list or print.
 class GetList():
-    #Same deal with file name, pass in as a string file path, extension added automatically.
-    #***single line
+
+#***Single line
     def simpleretrieve(file_name):
         list = []
-        # open file and read the content in a list
-        with open('{}.txt'.format(file_name), 'r') as filehandle:
+        # Open file and read the content in a list.
+        with open('{}'.format(file_name), 'r') as filehandle:
             for line in filehandle:
-                # remove linebreak which is the last character of the string
+                # Remove linebreak which is the last character of the string.
                 currentPlace = line[:-1]
-                # add item to the list
+                # Add item to the list.
                 list.append(currentPlace)
         return(list)
-    #***multi line
+
+#***Multi line
     def multiretrieve(file_name):
-        # define empty list
+        # Define empty list.
         list = []
-        # open file and read the content in a list
-        with open('{}.txt'.format(file_name), 'r') as filehandle:
+        # Open file and read the content in a list.
+        with open('{}'.format(file_name), 'r') as filehandle:
             filecontents = filehandle.readlines()
             for line in filecontents:
-                # remove linebreak which is the last character of the string
+                # Remove linebreak which is the last character of the string.
                 current_place = line[:-1]
-                # add item to the list
+                # Add item to the list.
                 list.append(current_place)
         return(list)
-    #pythonic aka one liner
+
+#***Pythonic aka one liner.
     def pythonicmultiretrive(file_name):
         list = []
-        # open file and read the content in a list
-        with open('{}.txt'.format(file_name), 'r') as filehandle:
-            list = [current_place.rstrip() for current_place in filehandle.readlines()]
+        # Open file and read the content in a list.
+        with open('{}'.format(file_name), 'r') as filehandle:
+            list = [
+                current_place.rstrip()
+                for current_place in filehandle.readlines()
+                ]
         return(list)
-    #***python only pickle module
+
+#***Python only pickle module
+# Typically use .pickle extension.
     def pklretrieve(file_name):
         list = []
-        with open('{}.data'.format(file_name), 'rb') as filehandle:
-            # read the data as binary data stream
+        with open('{}'.format(file_name), 'rb') as filehandle:
+            # Read the data as binary data stream.
             list = pickle.load(filehandle)
         return(list)
-    #***json format, very universal
+
+#***JSON format, very universal.
     def jretrieve(file_name):
         list = []
-        # open output file for reading
-        with open('{}.txt'.format(file_name), 'r') as filehandle:
+        # Open output file for reading.
+        with open('{}'.format(file_name), 'r') as filehandle:
             list = json.load(filehandle)
         return(list)
